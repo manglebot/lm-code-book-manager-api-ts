84a663462597cbab454e976fba2ceb1f951b0110
Ben <manglebot@users.noreply.github.com>
2023-11-24 18:22:47 +0000

    feat:add test to check if book already exists

--- src/controllers/books_controller.test.ts
+++ src/controllers/books_controller.test.ts
@@ -131,6 +131,17 @@ describe("POST /api/v1/books endpoint", () => {
 		// Assert
 		expect(res.statusCode).toEqual(400);
 	});
+
+	test("status code 409 Conflict -  for trying to save with an id that already exists", async () => {
+		// Act
+		const res = await request(app)
+			.post("/api/v1/books")
+			.send({ bookId: 1, title: "The Hobbit", author: "J. R. R. Tolkien", description: "Someone finds a nice piece of jewellery while on holiday." });
+
+		// Assert
+		expect(res.statusCode).toEqual(409);
+	});
+
 });
 
 
